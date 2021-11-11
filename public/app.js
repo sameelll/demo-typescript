1702502015068ce39d85d3ab9a21b3b4ec7d0d5f
Eren Akel <eren.akel.46@gmail.com>
2021-11-11 13:14:51 +0300

    client classes have been added

--- public/app.js
+++ public/app.js
@@ -1,4 +1,22 @@
 "use strict";
+// Classes
+var invoice = /** @class */ (function () {
+    function invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return invoice;
+}());
+var invOne = new invoice('mario', 'work on the mario website', 250);
+var invTwo = new invoice('luigi', 'work on the mario website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 // Getting form element
 var form = document.querySelector('.new-item-form');
 // Inputs
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
+// Classes
+class invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d:string, a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new invoice('mario', 'work on the mario website', 250);
+const invTwo = new invoice('luigi', 'work on the mario website', 300);
+
+let invoices: invoice[] = [];
+
+invoices.push(invOne);
+invoices.push(invTwo)
+
+console.log(invoices);
 // Getting form element
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
