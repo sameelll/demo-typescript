f6694a41ea10f0e6b0d2fb9f3c5f871c6127b877
Eren Akel <eren.akel.46@gmail.com>
2021-11-11 15:49:20 +0300

    some changes in the signs(£ --> $)

--- src/classes/listTemplate.ts
+++ src/classes/listTemplate.ts
@@ -6,8 +6,9 @@ export class listTemplate {
     ){}
 
     render(item: HasFormatter, heading: string, pos: 'start'|'end'){
+        
         const li = document.createElement('li');
-
+        
         const h4 = document.createElement('h4');
         h4.innerText = heading;
         li.append(h4);
